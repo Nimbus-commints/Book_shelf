7030823f468deda3c00064f1faa76e4d20c1d0d4
Nimbus <210294428+Nimbus-commints@users.noreply.github.com>
2025-08-07 18:01:19 -0500

    Adde button that reveals and hides the form to add a new book

--- book.js
+++ book.js
@@ -10,11 +10,24 @@ function Book(title, author, pages, readit){
 // t = title, a = author, p = pages, r = readit
 function addBookToLibrary(t,a,p,r) {
     const newBook = new Book(t,a,p,r);
-    const bookId = crypto.randomUUID()
-    return myLibrary.push([newBook,bookId])
+    const bookId = crypto.randomUUID();
+    return myLibrary.push([newBook,bookId]);
 }
 
 
-document.addEventListener('DOMContentLoaded',()=>{
-    
+
+const showFormBtnVar = document.getElementById('showFormBtn');
+const myFormVar = document.getElementById('myForm');
+
+showFormBtnVar.addEventListener('click',function() {
+    console.log('apretado');
+    if (myFormVar.style.display === '') {
+        myFormVar.style.display = 'block';
+        showFormBtnVar.textContent = "HIDE FORM";
+        console.log('apretado 1');
+    } else {
+        myFormVar.style.display = '';
+        showFormBtnVar.textContent = 'NEW BOOK';
+        console.log('apretado 2');
+    }
 })
